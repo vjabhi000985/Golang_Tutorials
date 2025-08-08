ef60296257ceabb62bfbc2f6f34476c7fb32e1e0
vjabhi00985 <princeabhi00985@gmail.com>
2025-08-08 14:46:26 +0530

    Multiple constant Declaration added

--- constants.go
+++ constants.go
@@ -3,7 +3,22 @@ import (
 	"fmt"
 )
 
+/*
+	`const` keyword declares the variables "constant". It is unchangeable and read-only.
+	# Syntax: `const CONSTNAME type = value` // Typed constant
+	# Syntax: `const CONSTNAME = value` // Untyped constant
+	# General Naming Rules:
+	1. Constants should be usually written in Uppercase letters.
+	2. It can be declared both inside or outside of a function. 
+*/
 const PI float32 = 3.14 
+
+// Multiple constant declaration using Blocks
+const (
+	A int = 1
+	B string = "Abhi"
+	C float32 = 32.4
+)
 
 func contanstTest(){
 	var radius float32
