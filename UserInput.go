5dd98ddfbecdffe90003e6b02cc5600dc7bb66d0
vjabhi00985 <princeabhi00985@gmail.com>
2025-08-10 21:53:21 +0530

    User Input Notes added

---
+++ UserInput.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func UserInput() {
+	var name string 
+	var currentSalary float32
+
+
+	// Scan()
+	/*
+		The Scan() function takes input from the user. However, it can only take input values up to a space.
+	*/
+	fmt.Println("Please Enter your name: ")
+	// fmt.Scan(&name) // Bittu Kumar
+	// fmt.Println("My name is",name) // Bittu
+	fmt.Scan(&name,&currentSalary)
+
+	fmt.Println("My name is",name) // Bittu
+	fmt.Println("My current salary is",currentSalary) // 12000.00
+	// Multiple Input using Scan()
+
+	// Scanln()
+
+	//Scanf()
+}
--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	// stringManipulation()
 	// DrawPattern()
 	// Operators()
-	FormattedStringsTutorial()
+	// FormattedStringsTutorial()
+	UserInput()
 }
