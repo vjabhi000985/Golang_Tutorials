88f26da7a613c23ac494adc6677969edf8553058
vjabhi00985 <princeabhi00985@gmail.com>
2025-08-12 13:52:14 +0530

    if else notes updated

--- GreatestNumber.go
+++ GreatestNumber.go
@@ -5,19 +5,31 @@ import (
 )
 
 func GreatestNumber(){
-	var x,y int
+	var x,y,z int
 
-	fmt.Print("Enter first number: ")
-	fmt.Scan(&x)
+	x = 19
+	y = 17
+	z = 15
 
-	fmt.Print("\nEnter second number: ")
-	fmt.Scan(&y)
-
-	if x > y {
-		fmt.Printf("\n%d is greater", x)
-	} else if x < y{
-		fmt.Printf("\n%d is greater\n", y)
-	} else{
-		fmt.Printf("\n%d and %d are equal.", x, y)
+	if x > y && x > z{
+		fmt.Println(x,"is the greatest number.")
+	}else if y > z{
+		fmt.Println(y,"is the greatest number.")
+	}else{
+		fmt.Println(z,"is the greatest number.")
 	}
+	// Greatest among two number
+	// fmt.Print("Enter first number: ")
+	// fmt.Scan(&x)
+
+	// fmt.Print("\nEnter second number: ")
+	// fmt.Scan(&y)
+
+	// if x > y {
+	// 	fmt.Printf("\n%d is greater", x)
+	// } else if x < y{
+	// 	fmt.Printf("\n%d is greater\n", y)
+	// } else{
+	// 	fmt.Printf("\n%d and %d are equal.", x, y)
+	// }
 }
